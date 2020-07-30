597d91e75679c76e6979d578b7a8db1ba87399e1
Puneethnaik <puneethn250@gmail.com>
2020-07-30 11:25:54 +0530

    AppBar show header text

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -4,8 +4,8 @@ import './AppBar.css';
 export default class AppBar extends React.Component {
     render() {
         return (
-            <div class="app-bar-container">
-                <div class="app-bar-header">{this.props.header}</div>
+            <div className="app-bar-container">
+                <div className="app-bar-header">{this.props.header}</div>
             </div>
         )
     }
