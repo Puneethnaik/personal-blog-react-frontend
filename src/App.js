94e0894dd4ac562307bb890e87a573ccea31e678
Puneethnaik <puneethn250@gmail.com>
2020-07-30 11:25:27 +0530

    Included the ContactMeSideBar and ContactMeHorizontalBar in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 //component imports
 import AppBar from './components/AppBar/AppBar';
 import AppBody from './components/AppBody/AppBody';
+import ContactMeHorizontalBar from './components/ContactMeHorizontalBar/ContactMeHorizontalBar';
+import ContactMeSideBar from './components/ContactMeSideBar/ContactMeSideBar';
 
 function App() {
   return (
       <div>
         <AppBar header="puneethnnaik.com" />
         <AppBody />
+        <ContactMeHorizontalBar />
+        <ContactMeSideBar />
       </div>
   );
 }
